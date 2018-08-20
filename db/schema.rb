cc55350e31008869c9ea0d43a8b0d549ec4b4185
miki matoba <miki@ekohe.com>
2018-08-20 10:38:49 +0900

    association user and files

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820010657) do
+ActiveRecord::Schema.define(version: 20180820011822) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.         "user"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
